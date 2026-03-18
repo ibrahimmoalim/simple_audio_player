22c5606fd7621412391af5f464c284c25185a0fa
ibrahimmoalim <ibrahimmoalim651@gmail.com>
2026-03-18 10:30:58 +0300

    modify

--- player.py
+++ player.py
@@ -1,11 +1,11 @@
 import warnings
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 
-import os
-import pygame
 import sys
-
+import os
 from pathlib import Path
+
+import pygame
 
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QLabel,
                              QGridLayout, QPushButton, QFileDialog)
@@ -33,7 +33,6 @@ class MainWindow(QMainWindow):
         self.pause = QPushButton("Pause", self)
         self.is_paused = False
         self.skip = QPushButton("Skip", self)
-        # self.stop = QPushButton("Stop", self)
         self.selected_folder = QLabel("")
         self.playing = QLabel("")
         self.error_text = QLabel("")
@@ -56,7 +55,6 @@ class MainWindow(QMainWindow):
         grid.addWidget(self.play, 0, 1)
         grid.addWidget(self.pause, 0, 2)
         grid.addWidget(self.skip, 0, 3)
-        # grid.addWidget(self.stop, 0, 4)
         grid.addWidget(self.selected_folder, 2, 1)
         grid.addWidget(self.playing, 4, 1)
         grid.addWidget(self.error_text, 6, 1)
@@ -65,7 +63,6 @@ class MainWindow(QMainWindow):
         self.play.setProperty('class', 'play')
         self.pause.setProperty('class', 'pause')
         self.skip.setProperty('class', 'skip')
-        # self.stop.setProperty('class', 'stop')
 
         grid.setAlignment(Qt.AlignTop)
 
@@ -75,27 +72,23 @@ class MainWindow(QMainWindow):
                 font-size: 16px;
                 font-family: Arial;
             }
-                           
+
             QPushButton[class='choose'] {
                 background-color: rgb(50, 180, 255);
             }
-            
+
             QPushButton[class='play'] {
                 background-color: lime;
             }
-                           
+
             QPushButton[class='pause'] {
                 background-color: rgb(155, 0, 255);
             }
-            
+
             QPushButton[class='skip'] {
                 background-color: rgb(250, 255, 0);
             }
-                           
-            QPushButton[class='stop'] {
-                background-color: red;
-            }
-                           
+
             QLabel {
                 font-weight: bold;
                 font-size: 24px;
@@ -107,12 +100,10 @@ class MainWindow(QMainWindow):
         self.play.setCursor(Qt.PointingHandCursor)
         self.pause.setCursor(Qt.PointingHandCursor)
         self.skip.setCursor(Qt.PointingHandCursor)
-        # self.stop.setCursor(Qt.PointingHandCursor)
 
         self.play.clicked.connect(self.play_audio)
         self.pause.clicked.connect(self.pause_audio)
         self.skip.clicked.connect(self.skip_audio)
-        # self.stop.clicked.connect(self.stop_audio)
         self.choose.clicked.connect(self.choose_folder)
 
 
@@ -123,10 +114,10 @@ class MainWindow(QMainWindow):
             self.selected_folder.setText(f"Selected folder: {self.folder_path}")
         except TypeError:
             return
-    
+
     def play_audio(self):
         try:
-            
+
             pygame.mixer.init()
             self.folder = self.folder_path
             self.audio_files = os.listdir(self.folder)
@@ -141,14 +132,12 @@ class MainWindow(QMainWindow):
         except FileNotFoundError:
             self.error_text.setText("Folder not found")
         # get any other error and display it as readable error text
-        except Exception as e:
-            self.error_text.setText(str(e))
 
     def pause_audio(self):
         if not pygame.mixer.get_init():
             self.error_text.setText("No audio playing!")
             return
-        
+
         if pygame.mixer.music.get_busy():
             pygame.mixer.music.pause()
             self.pause.setText("Resume")
@@ -165,60 +154,43 @@ class MainWindow(QMainWindow):
             # uncomment lines below to disable looping
             # if self.index >= len(self.audio_files):
             #     return
-            
+
             file = self.audio_files[self.index]
             pygame.mixer.music.load(self.folder + file)
             pygame.mixer.music.play()
             self.playing.setText(f"Playing Now: {file}")
-            
+
             self.index += 1
 
             if self.index >= len(self.audio_files):
                 self.index = 0
-            
+
         except pygame.error:
             self.error_text.setText("Please select a folder with only audio files!")
             self.playing.clear()
-        except Exception as e:
-            self.error_text.setText(str(e))
 
 
     def check_audio(self):
         # if audio is paused do not go to next file
         if self.is_paused:
             return
-        
+
         if not pygame.mixer.music.get_busy():
             self.play_next()
-            
-    
+
+
     def skip_audio(self):
         if not pygame.mixer.get_init():
             self.error_text.setText("No audio playing!")
             return
-        
+
         if pygame.mixer.music.get_busy():
             pygame.mixer.music.stop()
             self.play_next()
         else:
             self.error_text.setText("No audio playing!")
 
-  
-    def stop_audio(self):
-        if not pygame.mixer.get_init():
-            self.error_text.setText("No audio playing!")
-            return
-        
-        # if pygame.mixer.music.get_busy():
-        #     pygame.mixer.music.stop()
-        #     self.timer.stop()
-        #     self.index = 0
-        #     self.playing.clear()
-        #     self.play.setText("Play")
-        #     self.play.setStyleSheet("background-color: lime;")
-        # else:
-        #     self.error_text.setText("No audio playing!")
-    
+
 
 def main():
     app = QApplication(sys.argv)
